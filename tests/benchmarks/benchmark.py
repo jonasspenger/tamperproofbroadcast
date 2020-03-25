1affaa4d17bc16ffcc9c033b3440c13c777f6816
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-03-25 16:19:58 +0100

    Update benchmark.py

--- tests/benchmarks/benchmark.py
+++ tests/benchmarks/benchmark.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../tamperproofbroadcast"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 
 from google.cloud import storage
 import logging

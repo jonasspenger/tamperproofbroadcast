e4d2494f9e2318e5d322e5eb200953603970b513
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-03-26 15:56:08 +0100

    init

--- src/etcd.py
+++ src/etcd.py
@@ -3,8 +3,8 @@ import logging.config
 import threading
 import tempfile
 import binascii
+import retrying
 import port_for
-import backoff
 import logging
 import pickle
 import shutil
@@ -26,7 +26,7 @@ class _ETCDBroadcast(module.Module):
         self.queue = queue.Queue(maxsize=queuesize)
         self.cancel = None
 
-    @backoff.on_exception(backoff.expo, Exception)
+    @retrying.retry(wait_random_min=100, wait_random_max=2000, stop_max_delay=120000)
     def broadcast(self, message):
         self.etcdclient.put("broadcast", self._pack(message))
 
